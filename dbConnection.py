f00de94681af24882f3ba426c9d916bf457d1614
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-02-14 17:23:02 +0500

    added env

--- dbConnection.py
+++ dbConnection.py
@@ -1,6 +1,16 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-client = MongoClient('mongodb+srv://hassaanqaisar2:8BhDrTI7uKnKhjsL@cluster0.1fxixvl.mongodb.net/?retryWrites=true&w=majority')
+# Load environment variables from .env file
+load_dotenv()
+
+# Get MongoDB URI from environment variable
+mongodb_uri = os.getenv("MONGODB_URI")
+
+client = MongoClient(mongodb_uri)
+
+# client = MongoClient('mongodb+srv://hassaanqaisar2:8BhDrTI7uKnKhjsL@cluster0.1fxixvl.mongodb.net/?retryWrites=true&w=majority')
 
 db = client['Psl_data']
 collection = db['tweets']
@@ -11,9 +21,9 @@ documents = collection.find({"hashtags": "psl7"})
 # Loading data from first 50 documents
 tweets = []
 for i, doc in enumerate(documents):
-    if i == 5000:
+    if i == 4000:
         break
     tweets.append(doc['tweet'])
 
 # Print the length of the 'tweets' array
-print(f"Number of tweets: {len(tweets)}")
+# print(f"Number of tweets: {len(tweets)}")
